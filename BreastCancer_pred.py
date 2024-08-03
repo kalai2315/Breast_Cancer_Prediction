b2da0491af6e957541507e061902a5f3c0fd4c7b
Kalaiselvi Maheshkumar <152251807+kalai2315@users.noreply.github.com>
2024-08-03 10:47:50 +0530

    Update BreastCancer_pred.py

--- BreastCancer_pred.py
+++ BreastCancer_pred.py
@@ -6,7 +6,7 @@ from streamlit_option_menu import option_menu
 
 # Load the trained model
 #model_filename = 'F:\project\BreastCancer_project\svm_model.pkl'
-with open(svm_model.pkl, 'rb') as file:
+with open('svm_model.pkl', 'rb') as file:
     svm_model = pickle.load(file)
 
 
